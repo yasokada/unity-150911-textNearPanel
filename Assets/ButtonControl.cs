c75be32962de7ffd8bf34a42769a944fe413dfee
y_okada <yasokada@gmail.com>
2015-09-11 19:30:10 +0900

    impl ButtonClick()

--- Assets/ButtonControl.cs
+++ Assets/ButtonControl.cs
@@ -1,19 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ButtonControl : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	public Canvas myCanvas;
+	public GameObject myPanel;
 	
+	void drawTextOnTheLeftOfPanel(float val, bool atBottom) {
+		RectTransform panelRect = myPanel.GetComponent<RectTransform> ();
+		float width = panelRect.rect.width;
+		float height = panelRect.rect.height;
+		
+		RectTransform canvasRect = myCanvas.GetComponent<RectTransform> ();
+		
+		// Bottom Left
+		Vector3 pos;
+		pos = myPanel.transform.position;
+		pos.x -= width * 0.5f * canvasRect.localScale.x;
+		if (atBottom) {
+			pos.y -= height * 0.5f * canvasRect.localScale.y;
+		} else {
+			pos.y += height * 0.5f * canvasRect.localScale.y;
+		}
+		
+		GameObject BottomLeftGO = new GameObject ();
+		BottomLeftGO.transform.parent = myPanel.transform;
+		BottomLeftGO.name = "Text";
+		BottomLeftGO.transform.position = pos;
+		BottomLeftGO.transform.localScale = new Vector3 (1f, 1f, 1f);
+		Text BottomLeftText = BottomLeftGO.AddComponent<Text> ();
+		BottomLeftText.text = val.ToString ();
+		BottomLeftText.font = Resources.GetBuiltinResource (typeof(Font), "Arial.ttf") as Font;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void ButtonClick() {
 		Debug.Log ("button click");
+		drawTextOnTheLeftOfPanel (3.1415f, /* atBottom=*/ true);
+		drawTextOnTheLeftOfPanel (2.7182f, /* atBottom=*/ false);
 	}
 }
